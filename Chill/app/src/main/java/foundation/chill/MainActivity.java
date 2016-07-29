fb4f2c0f1608a132642ccf1e9b85a3752668bc82
Sam Siu <samsiu@Sams-MacBook-Pro-2.local>
2016-07-28 20:13:37 -0700

    Buttons: Set Click Listeners to Change Text Color on Click

--- Chill/app/src/main/java/foundation/chill/MainActivity.java
+++ Chill/app/src/main/java/foundation/chill/MainActivity.java
@@ -1,5 +1,6 @@
 package foundation.chill;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     TextView snowFallTextView;
     TextView temperatureTextView;
     TextView elevationTextView;
+    TextView locationTextView;
+    TextView locationDetailTextView;
+    TextView locationHypehnTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +42,9 @@ public class MainActivity extends AppCompatActivity {
         snowFallTextView = (TextView)findViewById(R.id.snowfall_textView);
         temperatureTextView = (TextView)findViewById(R.id.temperature_textView);
         elevationTextView = (TextView)findViewById(R.id.elevation_textView);
+        locationTextView = (TextView)findViewById(R.id.location1_textView);
+        locationDetailTextView = (TextView)findViewById(R.id.location2_textView);
+        locationHypehnTextView = (TextView)findViewById(R.id.locationHyphen_textView);
     }
 
     private void initColorButtons(){
@@ -54,15 +61,23 @@ public class MainActivity extends AppCompatActivity {
                 switch(view.getId()){
                     case R.id.color1_button:
                         Log.d(TAG, "Button 1 Clicked");
+                        int colorGrey = ContextCompat.getColor(MainActivity.this, R.color.colorGrey);
+                        setTextColor(colorGrey);
                         break;
                     case R.id.color2_button:
                         Log.d(TAG, "Button 2 Clicked");
+                        int colorNeon = ContextCompat.getColor(MainActivity.this, R.color.colorNeon);
+                        setTextColor(colorNeon);
                         break;
                     case R.id.color3_button:
                         Log.d(TAG, "Button 3 Clicked");
+                        int colorWhite = ContextCompat.getColor(MainActivity.this, R.color.colorWhite);
+                        setTextColor(colorWhite);
                         break;
                     case R.id.color4_button:
                         Log.d(TAG, "Button 4 Clicked");
+                        int colorBlack = ContextCompat.getColor(MainActivity.this, R.color.colorBlack);
+                        setTextColor(colorBlack);
                         break;
                     default:
                         Log.d(TAG, "NOTHING CLICKED");
@@ -70,11 +85,19 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
-
-
-
     }
+
+    private void setTextColor(int color) {
+        snowFallTextView.setTextColor(color);
+        temperatureTextView.setTextColor(color);
+        elevationTextView.setTextColor(color);
+        locationTextView.setTextColor(color);
+        locationHypehnTextView.setTextColor(color);
+        locationDetailTextView.setTextColor(color);
+    }
+
+
+
 
 
 }
