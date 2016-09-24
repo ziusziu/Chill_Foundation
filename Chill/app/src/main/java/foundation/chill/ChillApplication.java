521011d0f9f7c4d6608de10a6a1e0e3c513627d9
Sam Siu <samsiu@Sams-MacBook-Pro-2.local>
2016-09-23 22:37:28 -0700

    Twitter: Added Fabric

--- Chill/app/src/main/java/foundation/chill/ChillApplication.java
+++ Chill/app/src/main/java/foundation/chill/ChillApplication.java
@@ -5,12 +5,20 @@ import android.app.Application;
 import com.adobe.creativesdk.aviary.IAviaryClientCredentials;
 import com.adobe.creativesdk.foundation.AdobeCSDKFoundation;
 
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
 import timber.log.Timber;
 
 /**
  * Created by samsiu on 8/25/16.
  */
 public class ChillApplication extends Application implements IAviaryClientCredentials {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "ldfJSw0ye1ZkilJVYVFDCInzV";
+    private static final String TWITTER_SECRET = "AboDmjYTm1yZ8PK9jS2VW9cNjq9mtfoHswx5b1V4nmk8NGgXiJ";
+
 
     private static String creativeSDKClientId;
     private static String creativeSDKClientSecret;
@@ -19,6 +27,8 @@ public class ChillApplication extends Application implements IAviaryClientCreden
     @Override
     public void onCreate() {
         super.onCreate();
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
 
         creativeSDKClientId = getResources().getString(R.string.adobe_clientID);
         creativeSDKClientSecret = getResources().getString(R.string.adobe_secret);
